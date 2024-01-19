6243c08481c9c7d68395e577846c12e8415b616f
Luiz Augusto Scarsi <luizaugustoscarsi@gmail.com>
2024-01-18 21:11:29 -0300

    feat: Add user struct

---
+++ api/src/models/User.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+// User represents a social media user
+type User struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Nick      string    `json:"nick,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
