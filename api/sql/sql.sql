9759fcfeae76fd31f3547b862d7dc7e21056aa30
Luiz Augusto Scarsi <luizaugustoscarsi@gmail.com>
2024-01-22 09:24:43 -0300

    feat: Create users table with .sql, docker and Makefile

---
+++ api/sql/sql.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS devbook;
+USE devbook;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+    id int auto_increment primary key,
+    name varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique,
+    password varchar(20) not null unique,
+    createdAt timestamp default current_timestamp()
+) ENGINE=INNODB;
