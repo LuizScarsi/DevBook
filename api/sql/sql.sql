e905975f83f7137011e5600ecc5405bc7611be87
Luiz Augusto Scarsi <luizaugustoscarsi@gmail.com>
2024-02-16 20:14:54 -0300

    feat: Add followers table to sql script

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -11,3 +11,17 @@ CREATE TABLE users(
     password varchar(100) not null,
     createdAt timestamp default current_timestamp()
 ) ENGINE=INNODB;
+
+CREATE TABLE followers(
+    user_id int not null,
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    follower_id int not null,
+    FOREIGN KEY (follower_id), 
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    primary key(user_id, follower_id)
+) ENGINE=INNODB;
